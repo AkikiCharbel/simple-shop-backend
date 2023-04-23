44a3bc90a169c20adaea8a56cbaba6171253f4c8
Charbel Akiki <charbelakiki1111@gmail.com>
2023-04-23 14:03:24 +0300

    Added Name to the user api Resource

--- app/Data/Resource/UserData.php
+++ app/Data/Resource/UserData.php
@@ -12,6 +12,7 @@ class UserData extends Data
     public function __construct(
         public string $id,
         public string $email,
+        public string $name,
         #[WithTransformer(DateTimeInterfaceTransformer::class)]
         public ?Carbon $created_at,
         public bool $is_admin
